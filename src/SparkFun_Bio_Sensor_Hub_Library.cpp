b246cc3de992ecdb216e3758427c873b393fabab
Chris Idema <chris.idema@electronsquare.com>
2020-12-10 12:42:59 +0100

    -round up delays to next multiple of freertos tick + 1 tick

--- src/SparkFun_Bio_Sensor_Hub_Library.cpp
+++ src/SparkFun_Bio_Sensor_Hub_Library.cpp
@@ -20,6 +20,11 @@ kk
 
 #include "SparkFun_Bio_Sensor_Hub_Library.h"
 
+#ifdef ARDUINO_ESP32_DEV
+	#define delay(x) delay(x+19)
+#endif
+
+
 SparkFun_Bio_Sensor_Hub::SparkFun_Bio_Sensor_Hub(uint16_t resetPin, uint16_t mfioPin, uint8_t address) { 
   
   _resetPin = resetPin; 
